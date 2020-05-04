61dbb42c13e2d3838b9c8107a1a3cec9f4043500
ToEndThePeace <bran.ramirez.don@gmail.com>
2020-05-04 15:30:20 -0700

    Finished MVP

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,34 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from "react";
+import styled from "styled-components";
+
+import Todo from "./Todo";
+
+const TodoList = (props) => {
+  const { todoList, toggleComplete } = props;
+
+  return (
+    <StyledList>
+      {todoList.map((todo, index) => {
+        return (
+          <Todo
+            key={todo.id}
+            todo={todo}
+            index={index}
+            toggleComplete={toggleComplete}
+          />
+        );
+      })}
+    </StyledList>
+  );
+};
+const StyledList = styled.section`
+  display: flex;
+  flex-flow: row wrap;
+  align-content: flex-start;
+  min-height: 100%;
+  width: 70%;
+  padding: 20px 3%;
+  background-color: slategrey;
+`;
+
+export default TodoList;
