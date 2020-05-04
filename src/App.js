3b7a2c7f11bba4f641dab000725702acd65acbc3
ToEndThePeace <bran.ramirez.don@gmail.com>
2020-05-04 13:06:27 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
-import React from 'react';
+import React from "react";
+
+import styled from "styled-components";
+import { v4 as uuid } from "uuid";
+
+class Todo {
+  constructor(name) {
+    this.name = name;
+    this.id = uuid();
+    this.completed = false;
+  }
+}
+const initialTodoList = [new Todo("Get Crunk"), new Todo("Party On")];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -6,11 +18,15 @@ class App extends React.Component {
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
+      <StyledApp>
         <h2>Welcome to your Todo App!</h2>
-      </div>
+      </StyledApp>
     );
   }
 }
+
+const StyledApp = styled.div`
+
+`;
 
 export default App;
